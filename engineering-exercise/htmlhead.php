cd2e4fbab1b903bb0f33c645b11fc3e1b84ce0e4
SMaloney2017 <SMaloney2017@my.fit.edu>
2022-06-20 13:01:17 -0400

    add reusable html-head to share styling across pages

---
+++ engineering-exercise/htmlhead.php
@@ -0,0 +1,25 @@
+<!-- reusable html-head to share styling across multiple pages -->
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width"/>
+
+    <title>Qgiv Engineering Exercise</title>
+
+    <link rel="stylesheet" type="text/css" href="https://secure.qgiv.com/resources/admin/css/application.css"/>
+
+    <style type="text/css">
+        .container {
+            max-width: 1200px;
+            margin: 0 auto;
+        }
+
+        .logo-header {
+            padding: 2em;
+        }
+
+        .logo {
+            margin: 0 auto;
+            min-height: 80px;
+        }
+    </style>
+</head>
--- engineering-exercise/index.php
+++ engineering-exercise/index.php
@@ -38,30 +38,10 @@
 <html class="no-js lt-ie9" lang="en"> <![endif]-->
 <!--[if gt IE 8]><!-->
 <html class="no-js" lang="en"> <!--<![endif]-->
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-    <meta name="viewport" content="width=device-width"/>
 
-    <title>Qgiv Engineering Exercise</title>
-
-    <link rel="stylesheet" type="text/css" href="https://secure.qgiv.com/resources/admin/css/application.css"/>
-
-    <style type="text/css">
-        .container {
-            max-width: 1200px;
-            margin: 0 auto;
-        }
-
-        .logo-header {
-            padding: 2em;
-        }
-
-        .logo {
-            margin: 0 auto;
-            min-height: 80px;
-        }
-    </style>
-</head>
+<!-- we can reuse the header to share style across multiple pages -->
+<?php
+require 'htmlhead.php' ?>
 
 <body>
 <section class="container">
