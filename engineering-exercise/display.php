b9e7c549e57a1736675214c9b62fd6b3e0eded6d
SMaloney2017 <SMaloney2017@my.fit.edu>
2022-06-21 16:27:02 -0400

    Factored out repetitive code into GenerateHtmlMethods.php, renamed file from display.php to Display.php

--- engineering-exercise/display.php
+++ engineering-exercise/display.php
@@ -1,14 +1,14 @@
 <?php
 
-require './database/Connection.php';
-require './user/User.php';
+require 'GenerateHtmlMethods.php';
+require './Database/Connection.php';
 
 /* max rows for pagination */
 const MAX_ROWS = 15;
 
 /* attributes for dynamically building menu's and headers,
    as well as verifying that the selected ordering is valid */
-const USER_ATTRIBUTES = [
+const USER_ATTRIBUTES_BASIC = [
     'user_id',
     'email',
     'first',
@@ -23,158 +23,6 @@ const TRANSACTION_ATTRIBUTES = [
     'status',
     'payment_method',
 ];
-
-/**
- * Helper function for building radio buttons,
- * where name is the name of the variable,
- * value is the reference to the variable,
- * and label is the actual value.
- *
- * @param string $name
- * @param string $value
- * @param string $label
- * @return void
- */
-function buildRadioButton(string $name, string $value, string $label): void
-{
-    echo "<label for=$name>" . ucfirst($label) . "</label>\n";
-    echo "<input type='radio' name=$name value=$label" . ($label == $value ? ' checked' : '') . ">\n";
-}
-
-/**
- * Helper function for building navigation buttons for paginated
- * results. Variables passed in are used to build an url which
- * retains the query as the page reloads.
- *
- * @param int $page
- * @param int $page_count
- * @param string $radio
- * @param string $search
- * @param string $sorting
- * @param string $order
- * @return void
- */
-function buildNavigation(
-    int $page,
-    int $page_count,
-    string $radio,
-    string $search,
-    string $sorting,
-    string $order
-): void {
-    /* get base-url path */
-    $base_url = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-    /* if not viewing the first page of results */
-    if ($page > 1) {
-        $query = [
-            'radio' => $radio,
-            'page' => $page - 1,
-            'search' => $search,
-            'sorting' => $sorting,
-            'order' => $order
-        ];
-        /* build a new url retaining query (view, search-value and decremented page-number) */
-        $url = $base_url . "?" . http_build_query($query);
-        /* link to previous page of results */
-        echo "<a href='$url'>Previous</a>\t";
-    }
-
-    /* if there are remaining pages to view */
-    if ($page < $page_count) {
-        $query = [
-            'radio' => $radio,
-            'page' => $page + 1,
-            'search' => $search,
-            'sorting' => $sorting,
-            'order' => $order
-        ];
-        /* build a new url retaining query (view, search-value and incremented page-number) */
-        $url = $base_url . "?" . http_build_query($query);
-        /* link to next page of results */
-        echo "<a href='$url'>Next</a>";
-    }
-}
-
-/**
- * Helper function which builds the menu entries used to select
- * which attribute to use as the query's sorting value.
- *
- * @param string $sorting
- * @param array $attributes
- * @return void
- */
-function buildMenuEntries(string $sorting, array $attributes): void
-{
-    echo "<label for='sorting'>Sort by:\n</label>";
-    echo "<select id='sorting' name='sorting'>";
-    foreach ($attributes as $a) {
-        echo "<option value=$a" . ($sorting == $a ? " selected" : "") . ">$a</option>";
-    }
-    echo "</select>\n";
-}
-
-/**
- * Helper function which builds a text-field used to narrow results.
- * Searching users finds similar values to user_id, email, first-name, and last-name.
- * Searching transactions finds similar values to transaction_id and user_id.
- *
- * @param string $value
- * @return void
- */
-function buildSearchField(string $value): void
-{
-    echo "<label for='search'>Search:\n</label>";
-    echo "<input type='text' id='search' name='search' value=$value>\n";
-    echo "<input type='submit'>\n";
-}
-
-/**
- * Helper function which builds the table-header.
- *
- * @param array $attributes
- * @return void
- */
-function buildTableHeader(array $attributes): void
-{
-    echo "<thead><tr class='ui-secondary-color'>";
-    foreach ($attributes as $a) {
-        echo "<th class='ui-secondary-color'>$a</th>";
-    }
-    echo "</tr></thead>";
-}
-
-/**
- * Helper function which builds the table for the queried results.
- *
- * @param array $results
- * @param array $attributes
- * @return void
- */
-function buildTableEntries(array $results, array $attributes): void
-{
-    echo "<tbody>";
-    if (!empty($results)) {
-        /* for each item */
-        foreach ($results as $row) {
-            echo "<tr class='data-row'>";
-            foreach ($attributes as $a) {
-                if ($a == 'timestamp') {
-                    $formatted_date = date('m/d/Y', strtotime($row[$a]));
-                    echo "<td>$formatted_date</td>";
-                } else {
-                    if ($a == 'user_id') {
-                        echo "<td><a href='userinfo.php?id=$row[$a]'>$row[$a]</a></td>";
-                    } else {
-                        echo "<td>$row[$a]</td>";
-                    }
-                }
-            }
-            echo "</tr>";
-        }
-    }
-    echo "</tbody>";
-}
 
 /* selected table - transactions or users */
 $radio = 'users';
@@ -203,7 +51,7 @@ if (isset($_GET['order'])) {
 /* verify sorting is a valid attribute to the selected table ->
    if sorting is NOT an attribute in the selected table,
    default to a common value: user_id */
-if (!in_array($sorting, $radio == 'users' ? USER_ATTRIBUTES : TRANSACTION_ATTRIBUTES)) {
+if (!in_array($sorting, $radio == 'users' ? USER_ATTRIBUTES_BASIC : TRANSACTION_ATTRIBUTES)) {
     $sorting = 'user_id';
     $_GET['sorting'] = 'user_id';
 }
@@ -229,7 +77,7 @@ $page_count = 0;
 $row_count = 0;
 $results = array();
 try {
-    /* create a connection to the database */
+    /* create a connection to the Database */
     $connection = new Connection();
     $pdo = $connection->getConnection();
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -289,7 +137,7 @@ try {
 
 <!-- we can reuse the header to share style across multiple pages -->
 <?php
-require 'htmlhead.php' ?>
+require 'GenerateHtmlHead.php' ?>
 
 <body>
 <form name='search' method='get'>
@@ -300,10 +148,10 @@ require 'htmlhead.php' ?>
     buildRadioButton("radio", $radio, "users");
     echo "<br><hr>";
 
-    /* if users radio button selected, build user menu-entries
+    /* if users radio button selected, build User menu-entries
        otherwise, build transaction menu-entries */
     if ($radio == 'users') {
-        buildMenuEntries($sorting, USER_ATTRIBUTES);
+        buildMenuEntries($sorting, USER_ATTRIBUTES_BASIC);
     } else {
         buildMenuEntries($sorting, TRANSACTION_ATTRIBUTES);
     }
@@ -313,26 +161,20 @@ require 'htmlhead.php' ?>
     buildRadioButton("order", $order, "DESC");
     echo "<br><hr>";
 
-    /* search text-field to narrow results by user-id, email, name, etc. */
+    /* search text-field to narrow results by User-id, email, name, etc. */
     buildSearchField($search);
 
     /* builds 'Previous' and 'Next' buttons for paginated results */
     buildNavigation($page, $page_count, $radio, $search, $sorting, $order);
     echo "<br><hr>";
-    ?>
 
-    <!-- begin building table which displays users -->
-    <table class='data-table'>
-        <?php
-        if ($radio == 'users') {
-            buildTableHeader(USER_ATTRIBUTES);
-            buildTableEntries($results, USER_ATTRIBUTES);
-        } else {
-            buildTableHeader(TRANSACTION_ATTRIBUTES);
-            buildTableEntries($results, TRANSACTION_ATTRIBUTES);
-        }
-        ?>
-    </table>
+    /* build selected table */
+    if ($radio == 'users') {
+        buildTable($results, USER_ATTRIBUTES_BASIC);
+    } else {
+        buildTable($results, TRANSACTION_ATTRIBUTES);
+    }
+    ?>
 </form>
 </body>
 </html>
