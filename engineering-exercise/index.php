57a22202bfaa7c40a89d3cd15e3d6515c0976ac0
SMaloney2017 <SMaloney2017@my.fit.edu>
2022-06-21 22:54:18 -0400

    Add checklist to ensure requirements are met

--- engineering-exercise/index.php
+++ engineering-exercise/index.php
@@ -1,31 +1,31 @@
 <?php
 /*
-- create a Database to hold transaction records and users
-- loads in a list of users from JSON: https://randomuser.me/api/?results=500&nat=us&exc=login,id,nat
-    - add unique users to a Database view
-    - as users are loaded, generate a transaction record tied to the User
-    - create a User class to work with the data
-        - use the class to format birthdate to the following format: January 1, 1990
-        - write a method to generate a transaction for the User
+- ✓ create a Database to hold transaction records and users
+- ✓ loads in a list of users from JSON: https://randomuser.me/api/?results=500&nat=us&exc=login,id,nat
+    - ✓ add unique users to a Database view
+    - ✓ as users are loaded, generate a transaction record tied to the User
+    - ✓ create a User class to work with the data
+        - ✓ use the class to format birthdate to the following format: January 1, 1990
+        - ✓ write a method to generate a transaction for the User
             - in addition to storing User details, transactions should have:
-                - a unique ID
-                - timestamp
-                - amount   
-                - status
-                - payment method (Visa, Mastercard, Discover, American Express, eCheck, or any other new payment method that pops up)
+                - ✓ a unique ID
+                - ✓ timestamp
+                - ✓ amount
+                - ✓ status
+                - ✓ payment method (Visa, Mastercard, Discover, American Express, eCheck, or any other new payment method that pops up)
         - anything else you think would be helpful for working with the data
 
-- view a view of transactions with data displayed
-- format transactions in the following format: MM/DD/YYYY
+- ✓ view a view of transactions with data displayed
+- ✓ format transactions in the following format: MM/DD/YYYY
 - default sorted by transaction ID
 
-- view a view of users with data displayed
-- clicking the User ID takes you to a view that displays that users details and associated transactions
+- ✓ view a view of users with data displayed
+- ✓ clicking the User ID takes you to a view that displays that users details and associated transactions
 
 - note: logic should be built without utilizing PHP libraries
 
-- bonus: add dynamic view sorting
-- bonus: add pagination
+- ✓ bonus: add dynamic view sorting
+- ✓ bonus: add pagination
 */
 
 ?>
